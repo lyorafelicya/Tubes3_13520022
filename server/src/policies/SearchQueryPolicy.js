2f07e9de28555c997ea61b45d7d705d96245dabb
HanselTanoto <hansel.tanoto05@gmail.com>
2022-04-29 08:42:31 +0700

    fix syntax error in search query policy

--- server/src/policies/SearchQueryPolicy.js
+++ server/src/policies/SearchQueryPolicy.js
@@ -28,70 +28,89 @@ module.exports = {
             // Hasil slicing dengan regex
             var dates = req.query.search.match(regexDate);
             var diseases = req.query.search.match(regexDisease);
+            var date = "";
+            var diseaseName = "";
     
             // mencari tanggal
-            // var regexDigits = /((?:\d{2}){1,2}|\d)/g;
-            // var date_digits = dates[0].match(regexDigits);
-            // var date
-            // if (date_digits.length == 3) {
-            //     if (date_digits[0].length == 4) {
-            //         date = date_digits[0] + '-' + date_digits[1] + '-' + date_digits[2];   
-            //     } else if (date_digits[0].length == 2) {
-            //         date = date_digits[2] + '-' + date_digits[1] + '-' + date_digits[0];
-            //     }
-            // } else if (date_digits.length == 2) {
-            //     var month;
-            //     if (date_digits[1].match(/[Jj]an(?:uar[yi])?/)) {
-            //         month = '01';
-            //     } else if (date_digits[1].match(/[Ff]eb(?:ruar[yi])?/)) {
-            //         month = '02';
-            //     } else if (date_digits[1].match(/[Mm]ar(?:(ch|et))?/)) {
-            //         month = '03';
-            //     } else if (date_digits[1].match(/[Aa]pr(?:il)?/)) {
-            //         month = '04';
-            //     } else if (date_digits[1].match(/[Mm](ay|ei)/)) {
-            //         month = '05';
-            //     } else if (date_digits[1].match(/[Jj]un[ei]?/)) {
-            //         month = '06';
-            //     } else if (date_digits[1].match(/[Jj]ul[yi]?/)) {
-            //         month = '07';
-            //     } else if (date_digits[1].match(/[Aa]ug(?:ust)?/)) {
-            //         month = '08';
-            //     } else if (date_digits[1].match(/[Aa]gu(?:stus)?/)) {
-            //         month = '09';
-            //     } else if (date_digits[1].match(/[Ss]ep(?:tember)?/)) {
-            //         month = '10';
-            //     } else if (date_digits[1].match(/[Oo][ck]t(?:ober)?/)) {
-            //         month = '11';
-            //     } else if (date_digits[1].match(/[Nn]ov(?:ember)?/)) {
-            //         month = '12';
-            //     } else if (date_digits[1].match(/[Dd]e[cs](?:ember)?)/)) {
-            //         month = '12';
-            //     }
-            //     if (date_digits[0].length == 4) {
-            //         date = date_digits[0] + '-' + month + '-' + this.addLeadingZeros(date_digits[2], 2);   
-            //     } else if (date_digits[0].length == 2) {
-            //         date = date_digits[2] + '-' + month + '-' + this.addLeadingZeros(date_digits[0], 2);
-            //     }
-            // }
-    
-            // // mencari nama penyakit
-            // var diseaseName;
-            // for (var i = 0; i < diseases.length; i++) {
-            //     if (!diseases[i].match(/[Jj]an(?:uar[yi])?/) && !diseases[i].match(/[Ff]eb(?:ruar[yi])?/) && !diseases[i].match(/[Mm]ar(?:(ch|et))?/) && !diseases[i].match(/[Aa]pr(?:il)?/) && !diseases[i].match(/[Mm](ay|ei)/) && !diseases[i].match(/[Jj]un[ei]?/) && !diseases[i].match(/[Jj]ul[yi]?/) && !diseases[i].match(/[Aa]ug(?:ust)?/) && !diseases[i].match(/[Aa]gu(?:stus)?/) && !diseases[i].match(/[Ss]ep(?:tember)?/) && !diseases[i].match(/[Oo][ck]t(?:ober)?/) && !diseases[i].match(/[Nn]ov(?:ember)?/) && !diseases[i].match(/[Dd]e[cs](?:ember)?)/)) {
-            //         diseaseName = diseases[i];
-            //         break;
-            //     }
-            // }
-            // // hapus spasi di awal dan akhir
-            // diseaseName = diseaseName.replace(/(^\s+|\s+$)/gm, '');
+            if (dates != null) {
+
+                var regexDigits = /((?:\d{2}){1,2}|\d)/g;
+                var date_digits = dates[0].match(regexDigits);
+                // var date
+                if (date_digits.length == 3) {
+                    if (date_digits[0].length == 4) {
+                        date = date_digits[0] + '-' + String(date_digits[1]).padStart(2, '0') + '-' + String(date_digits[2]).padStart(2, '0');   
+                    } else if (date_digits[0].length == 2) {
+                        date = date_digits[2] + '-' + String(date_digits[1]).padStart(2, '0') + '-' + String(date_digits[0]).padStart(2, '0');
+                    }
+                } else if (date_digits.length == 2) {
+                    var month;
+                    if (dates[0].search(/[Jj]an(?:uar[yi])?/) != -1) {
+                        month = '01';
+                    } else if (dates[0].search(/[Ff]eb(?:ruar[yi])?/) != -1) {
+                        month = '02';
+                    } else if (dates[0].search(/[Mm]ar(?:(ch|et))?/) != -1) {
+                        month = '03';
+                    } else if (dates[0].search(/[Aa]pr(?:il)?/) != -1) {
+                        month = '04';
+                    } else if (dates[0].search(/[Mm](ay|ei)/) != -1) {
+                        month = '05';
+                    } else if (dates[0].search(/[Jj]un[ei]?/) != -1) {
+                        month = '06';
+                    } else if (dates[0].search(/[Jj]ul[yi]?/) != -1) {
+                        month = '07';
+                    } else if (dates[0].search(/[Aa]ug(?:ust)?/) != -1) {
+                        month = '08';
+                    } else if (dates[0].search(/[Aa]gu(?:stus)?/) != -1) {
+                        month = '08';
+                    } else if (dates[0].search(/[Ss]ep(?:tember)?/ ) != -1) {
+                        month = '09';
+                    } else if (dates[0].search(/[Oo][ck]t(?:ober)?/) != -1) {
+                        month = '10';
+                    } else if (dates[0].search(/[Nn]ov(?:ember)?/) != -1) {
+                        month = '11';
+                    } else if (dates[0].search(/[Dd]e[cs](?:ember)?/) != -1) {
+                        month = '12';
+                    }
+                    if (date_digits[0].length == 4) {
+                        date = date_digits[0] + '-' + month + '-' + String(date_digits[1]).padStart(2, '0');   
+                    } else if (date_digits[0].length == 2) {
+                        date = date_digits[1] + '-' + month + '-' + String(date_digits[0]).padStart(2, '0');
+                    }
+                }
+            }
+            
+            if (diseases != null) {
+                // mencari nama penyakit
+                // var diseaseName = "";
+                for (var i = 0; i < diseases.length; i++) {
+                    if (!(/[Jj]an(?:uar[yi])?/).test(diseases[i]) && 
+                        !(/[Ff]eb(?:ruar[yi])?/).test(diseases[i]) && 
+                        !(/[Mm]ar(?:(ch|et))?/).test(diseases[i]) && 
+                        !(/[Aa]pr(?:il)?/).test(diseases[i]) && 
+                        !(/[Mm](ay|ei)/).test(diseases[i]) && 
+                        !(/[Jj]un[ei]?/).test(diseases[i]) && 
+                        !(/[Jj]ul[yi]?/).test(diseases[i]) && 
+                        !(/[Aa]ug(?:ust)?/).test(diseases[i]) && 
+                        !(/[Aa]gu(?:stus)?/).test(diseases[i]) && 
+                        !(/[Ss]ep(?:tember)?/).test(diseases[i]) && 
+                        !(/[Oo][ck]t(?:ober)?/).test(diseases[i]) && 
+                        !(/[Nn]ov(?:ember)?/).test(diseases[i]) && 
+                        !(/[Dd]e[cs](?:ember)?/).test(diseases[i])) {
+                        diseaseName = diseases[i];
+                        break;
+                    }
+                }
+                // hapus spasi di awal dan akhir
+                diseaseName = diseaseName.replace(/(^\s+|\s+$)/gm, '');
+            }
     
     
             // // NOTES:
             // // Final Result: date & diseaseName
 
-            req.body.diseaseName=diseases;
-            req.body.date=dates;
+            req.body.diseaseName=diseaseName;
+            req.body.date=date;
             console.log(req.body);
             next();
 
@@ -101,4 +120,8 @@ module.exports = {
             });
         }
     },
+
+    addLeadingZeros(num, totalLength) {
+        return String(num).padStart(totalLength, '0');
+    }
 }
